65fd82eab16b9e5273f728e3b4798a3df993aba0
Kevin Coto <kevincoto2002@gmail.com>
2024-07-24 13:30:19 -0400

    feat: icon board, up and down buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,190 @@
-import Image from "next/image";
+"use client"
+
+import { Button, Container, Grid, Icon, TextField } from "@mui/material";
+import { useState, ChangeEvent, useEffect } from "react";
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import PixiExponentialLine from "@/components/PixiExponentialLine";
+
 
 export default function Home() {
+  const [name, setName] = useState<string>('');
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false)
+  const [points, setPoints] = useState<number>(1000)
+  const [pointsToBet, setPointsToBet] = useState<number>(0)
+  const [multlipier, setMultlipier] = useState<number>(1.00)
+  const [time, setTime] = useState<{ hours: number; minutes: number }>(getCurrentTime());
+  const [value, setValue] = useState<number>(0);
+  const [pointPosition, setPointPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+
+  function generateRandomNumber(): number {
+    return +(Math.random() * (10 - 1) + 1).toFixed(2);
+  }
+
+  const handleStartBet = () => {
+    setValue(generateRandomNumber)
+  }
+
+  useEffect(() => {
+    const x = value * 10;
+    const y = 600 - Math.exp(value) * 10; // scaling the point
+    setPointPosition({ x, y });
+  }, [value]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getCurrentTime());
+    }, 60000); // Update every minute (60,000 milliseconds)
+
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    return { hours, minutes };
+  }
+
+
+  const handleName = (event: ChangeEvent<HTMLInputElement> ) => {
+    setName(event.target.value);
+  };
+  
+  const handleAccept = () => {
+    setIsGameStarted(!isGameStarted)
+  }
+  const handleDecrementPointsToBet = () => {
+    if(pointsToBet === 0) return;
+    setPointsToBet(prevState => prevState - 25)
+  }
+  const handleIncrementPointsToBet = () => {
+    if(pointsToBet >= points) return;
+    setPointsToBet(prevState => prevState + 25)
+  }
+  const handleDecrementMultiplier = () => {
+    if(multlipier === 1.00) return;
+    setMultlipier(prevState => prevState - 0.25)
+  }
+  const handleIncrementMultlipier = () => {
+    if(multlipier >= 10.00) return;
+    setMultlipier(prevState => prevState + 0.25)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+   <div className="py-10 px-20 h-screen bg-slate-900">
+  <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} className="h-3/4">
+  <Grid item xs={4} className="border rounded-xl bg-slate-800 border-slate-500">
+  {!isGameStarted &&
+  <Container className="h-full w-full flex flex-col items-center justify-evenly ">
+     <h1 className="text-2xl">Please insert your name</h1>
+     <TextField 
+     label="Name" 
+     variant="filled" 
+     color="secondary"
+     value={name}
+     onChange={handleName}
+     InputLabelProps={{
+      sx: {
+        color: 'white', // Label color
+      },
+    }}
+     sx={{
+      '& .MuiInputBase-input': {
+        color: 'white', // Change this to your desired color
+        backgroundColor: '#1f2329'
+      }
+    }}
+     />
+     <Button 
+     color="secondary" 
+     variant="contained" 
+     size="large"
+     disabled={!name}
+     onClick={handleAccept}
+     >
+      Accept
+    </Button>
+  </Container>
+}
+{isGameStarted &&
+    <Container className="h-full w-full flex flex-col">
+      <Container className="flex justify-center gap-4">
+          <Container className="up-down">
+              <Container onClick={handleDecrementPointsToBet} className="flex max-w-16 min-h-12 justify-center items-center rounded-2xl border border-gray-600">
+                <Icon fontSize="large" color="secondary" component={ArrowDropDownIcon}/>
+              </Container>
+              <Container className="flex flex-col justify-evenly items-center -mx-4">
+                <span className="text-sm text-gray-500 ">Points</span>
+                <Container className="flex justify-center max-w-20 min-w-20 bg-black py-2  rounded-2xl">
+                  <span className="text-lg font-semibold">{pointsToBet}</span>
+                </Container>
+              </Container>
+              <Container  onClick={handleIncrementPointsToBet}  className="flex max-w-16 min-h-12 justify-center items-center rounded-2xl border border-gray-600">
+                <Icon fontSize="large" color="secondary" component={ArrowDropUpIcon}/>
+              </Container>
+          
+          </Container>
+          <Container className="up-down">
+              <Container onClick={handleDecrementMultiplier} className="flex max-w-16 min-h-12 justify-center items-center rounded-2xl border border-gray-600">
+                <Icon fontSize="large" color="secondary" component={ArrowDropDownIcon}/>
+              </Container>
+              <Container className="flex flex-col justify-evenly items-center -mx-4">
+                <span className="text-sm text-gray-500 ">Multiplier</span>
+                <Container className="flex justify-center max-w-20 min-w-20 bg-black py-2  rounded-2xl">
+                  <span className="text-lg font-semibold">{multlipier}</span>
+                </Container>
+              
+              </Container>
+              <Container onClick={handleIncrementMultlipier}  className="flex max-w-16 min-h-12 justify-center items-center rounded-2xl border border-gray-600">
+                <Icon fontSize="large" color="secondary" component={ArrowDropUpIcon}/>
+              </Container>
+          
+          </Container>
+          
+      </Container>
+    </Container>
+}
+</Grid>
+  <Grid item xs={8} className="flex flex-col gap-4">
+    <Container component='nav' className="flex justify-between">
+   
+      <div className="icon-board">
+        <Icon component={MilitaryTechIcon} fontSize="large" color="secondary"></Icon>
+        <span className="mx-auto text-xl font-bold">{isGameStarted ? points: ''}</span>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="icon-board">
+        <Icon component={SentimentSatisfiedAltIcon} fontSize="large" color="secondary"></Icon>
+        <span className="mx-auto text-xl font-bold">{isGameStarted ? name : ''}</span>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="icon-board">
+        <Icon component={AlarmIcon} fontSize="large" color="secondary"></Icon>
+        <span className="mx-auto text-xl font-bold">{time.hours.toString().padStart(2, '0')}:{time.minutes.toString().padStart(2, '0')}</span>
       </div>
-    </main>
+   
+    </Container>
+    <div className="relative w-full h-full flex justify-center items-center">
+      <div className="relative">
+        <PixiExponentialLine value={value} />
+        <div className="absolute left-0 bottom-0 w-full h-0.5 bg-black"></div>
+        <div className="absolute left-0 bottom-0 h-full w-0.5 bg-black"></div>
+        <div
+          className="absolute w-2.5 h-2.5 bg-pink-500 rounded-full"
+          style={{ left: `${pointPosition.x}px`, top: `${pointPosition.y}px` }}
+        ></div>
+      </div>
+    </div>
+  </Grid>
+  </Grid>
+  <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <Grid item xs={7}>
+     Hola
+  </Grid>
+  <Grid item xs={5}>
+    Bye Bye
+  </Grid>
+  </Grid>
+   </div>
   );
 }
